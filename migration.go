cc69bf32725b1d19dec06ac175cc43b34d7a7632
houzhongjian <houzhongjian@kxkr.com>
2017-08-05 12:41:25 +0800

    数据库迁移工具

---
+++ migration.go
@@ -0,0 +1,62 @@
+package pgmigration
+
+import (
+	"reflect"
+	"log"
+	"fmt"
+	"unicode"
+	"strings"
+	"database/sql"
+)
+
+func Migration (values... interface{}, db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Printf("%+v\n", err)
+		return err
+	}
+	for _, v := range values {
+		esql := NewMigration(&v)
+		st, err := tx.Prepare(esql)
+		if err != nil {
+			log.Printf("%+v\n", err)
+			return err
+		}
+		_, err = st.Exec()
+		if err != nil {
+			log.Printf("%+v\n", err)
+			return err
+		}
+		return nil
+	}
+}
+
+func NewMigration(value *interface{}) string {
+	t := reflect.TypeOf(&value)
+	tableName := TableName(t.Elem().Name())
+	esql := fmt.Sprintf(`CREATE TABLE "%s" (`, tableName)
+	for i := 0; i < t.Elem().NumField(); i++ {
+		//字段名.
+		filed := t.Elem().Field(i).Tag.Get("db")
+		sArr := strings.Split(filed, ", ")
+		esql += fmt.Sprintf("%s, ", strings.Join(sArr, " "))
+	}
+	return strings.TrimSuffix(esql, ", ") + ")"
+}
+
+//返回表名.
+func TableName(name string) string {
+	tableName := ""
+	for k, v := range name {
+		if unicode.IsUpper(v) {
+			if k == 0 {
+				tableName += string(v)
+			} else {
+				tableName += fmt.Sprintf("_%s", string(v))
+			}
+		} else {
+			tableName += string(v)
+		}
+	}
+	return strings.ToLower(tableName)
+}
